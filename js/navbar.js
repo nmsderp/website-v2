0475ce8118d3940729a70ba664210cbd502a0b5b
nmsderp <130254323+nmsderp@users.noreply.github.com>
2024-04-19 21:46:10 -0400

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,6 @@
 let htmlb = `
-<h1>Dumo</h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<h1>nmsderp</h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 <a href="/">Home</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-<a href="/pages/projects.html">Projects</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 <a href="/pages/extra.html">Extras</a>
 `;
 
