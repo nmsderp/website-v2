6392a6a3836ccca7caf49b7f08c90b677cf972b3
nmsderp <130254323+nmsderp@users.noreply.github.com>
2024-07-07 18:24:48 -0400

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -1,5 +1,5 @@
 let htmlb = `
-<h1>nmsderp</h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<a href="https://nmsderp.xyz" target="_blank"><img src="https://nmsderp.xyz/gif.gif" alt="nmsderp.xyz" title="nmsderp.xyz"></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 <a href="/">Home</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 <a href="/pages/extra.html">Extras</a>
 `;
